ca71a2ce2a1f3ec66a099e9ee05179af828bec27
DavidjRoss <dav1d@shaw.ca>
2017-02-02 21:08:03 -0700

    Initial commit for Pathfinding

---
+++ Pathfinding/Dijkstras.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+class Dijkstras:
+	'Find the shortest path between two nodes retrieved from image'
+
+	# Will need to decide whether we will store start/end inside a node or just pass it
+	def __init__(self, nodeList, startNode, endNode):
+		self.unvistedNodes = nodeList
+		print("Initialized")
+
+	def testFunc(self):
+		print("test2") 
+
